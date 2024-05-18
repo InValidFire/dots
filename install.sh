2e34b686ba1d066f1df7b8dc08a6f7f11274d649
Riley Housden <riley.t.housden@gmail.com>
2024-05-18 00:28:42 -0400

    update install scripts

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/bash
 # WIP:
 # - set up way to run this from github via curl piping into bash
 # - automatically install and set KDE dracula theme
 
 # install dots via git
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-git clone --bare git@github.com:InValidFire/dots.git $HOME/.cfg
+alias config="/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+git clone --bare git@github.com:InValidFire/dots.git "$HOME"/.cfg
 config checkout
 config config --local status.showUntrackedFiles no
 
@@ -19,20 +20,23 @@ apt-get -qq --yes --force-yes install tmux  # terminal multiplexer
 
 # wsl stuff
 if [ -z "${WSL_DISTRO_NAME}" ]; then
-	IS_WSL = false
+  IS_WSL=false
 else
+  IS_WSL=true
+fi
 snap install bitwarden  # bitwarden :)
 snap connect bitwarden:password-manager-service
 
 # set shell to xonsh
-chsh -s /usr/bin/xonsh $USER
+chsh -s /usr/bin/xonsh "$USER"
 
 # install vim-plug
 apt-get -qq update && apt-get -qq --yes --force-yes install curl
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # run DE dependent setup script
-if [ $DESKTOP_SESSION = "plasma" ]; then
-		source ~/sh/plasma_setup.sh
+if [ "$DESKTOP_SESSION" = "plasma" ]; then
+    source ~/sh/plasma_setup.sh
 fi  # is this syntax correct?
+
